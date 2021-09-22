3c4ad4236deecfd84b7bfd586479db5365a3961b
kumarh1 <Hemant.Kumar@uhs.nhs.uk>
2021-09-22 22:34:48 +0100

    Add weather app component

--- src/typescripts/app.tsx
+++ src/typescripts/app.tsx
@@ -1,14 +1,8 @@
-import '../styles/index.scss';
 const React = require('react');
 const ReactDOM = require('react-dom');
+import WeatherApp from './components/WeatherApp';
 
-const WeatherApp = (props) => {
-
-    return (
-        <div>
-            <h1>Weather App</h1>
-        </div>
-    )
-}
-
-ReactDOM.render(<WeatherApp />, document.getElementById('root'));
+ReactDOM.render(
+    <WeatherApp />, 
+    document.getElementById('root')
+);
---
+++ src/typescripts/components/WeatherApp.tsx
@@ -0,0 +1,13 @@
+import '../../styles/index.scss';
+const React = require('react');
+
+const WeatherApp = (props) => {
+
+    return (
+        <div>
+            <h1>Weather App</h1>
+        </div>
+    );
+}
+
+export default WeatherApp;
